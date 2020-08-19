bb1abf0adbfd6c326dd848a9cb3c7a7d7918463d
sayefdeen <sayefdeen.alrawad@gmail.com>
2020-08-19 10:57:45 +0300

    fix all concatenations

--- class-00/lab-a/lab/js/app.js
+++ class-00/lab-a/lab/js/app.js
@@ -39,7 +39,7 @@ const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = "img/" + name + ".jpg";
+  this.path = `img/${name}.jpg`;
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -84,12 +84,7 @@ function handleClick(event) {
     if (event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
       console.log(
-        event.target.id +
-          " has " +
-          allProducts[i].votes +
-          " votes in " +
-          allProducts[i].views +
-          " views"
+        `${event.target.id} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`
       );
     }
   }
@@ -101,13 +96,7 @@ function handleClick(event) {
 function showList() {
   for (let i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement("li");
-    liEl.textContent =
-      allProducts[i].name +
-      " has " +
-      allProducts[i].votes +
-      " votes in " +
-      allProducts[i].views +
-      " views";
+    liEl.textContent = `${allProducts[i].name} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`;
     list.appendChild(liEl);
   }
 }
